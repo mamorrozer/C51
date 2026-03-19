df9429db8c9e36df3680e6bf7b477fd9fa019254
song <1714536371@qq.com>
2026-03-19 13:07:32 +0800

    newproject

---
+++ 3-1 按键控制led/main.c
@@ -0,0 +1,47 @@
+#include <REGX52.H>
+
+void Delay(unsigned int xms);
+unsigned char LEDNum;
+	
+void main()
+{
+
+	P2 = 0XFE;
+	while(1)
+	{
+		if(P3_1==0)
+		{
+			Delay(20);
+			while(P3_1==0);
+			Delay(20);
+			
+			LEDNum++;
+			if(LEDNum>=8)LEDNum=0;
+			P2=~(0x01<<LEDNum);
+		}
+		if(P3_0==0)
+		{
+			Delay(20);
+			while(P3_0==0);
+			Delay(20);
+			
+			LEDNum--;
+			if(LEDNum<=0)LEDNum=8;
+			P2=~(0x01<<LEDNum);
+		}
+	}
+}
+
+void Delay(unsigned int xms)		//@12.000MHz
+{
+	unsigned char i, j;
+	while(xms--)
+	{
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
